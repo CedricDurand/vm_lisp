afd542c4218ed5720c35d9eafc50c8a2374ca6b1
CedricDurand <cedricdurand13@sfr.fr>
2019-01-20 21:39:54 +0100

    merge

--- chargeur.lisp
+++ chargeur.lisp
@@ -20,7 +20,7 @@
 (defun compi-if(exp)
 	( let ((sinon (gensym "sinon"))(fin (gensym "fin")))
 	(append (compilation (car exp))
-		'((CMP :R2 (:CONST nil))) ;; si ça n'a pas jump dans la compa
+		'((CMP R2 (CONST nil))) ;; si ça n'a pas jump dans la compa
 	    `((JEQ (@ ,sinon)))
 		(compilation (cadr exp))
 		`((JMP (@ ,fin)))
@@ -34,13 +34,13 @@
 (defun compi-compa (exp)
 	(let ((op (car exp))(else_ (gensym "else")))
 	(append (compilation (list (cadr exp)))
-			'((PUSH :R0))
+			'((PUSH R0))
 			(compilation (list (caddr exp)))
-			'((PUSH :R0))
-		    '((POP :R0))
-		    '((POP :R1))
-		    '((CMP :R1 :R0))
-		    '((MOVE (:CONST T) :R2)) ;; on met à T pour la future comparaison dans le if
+			'((PUSH R0))
+		    '((POP R0))
+		    '((POP R1))
+		    '((CMP R1 R0))
+		    '((MOVE (CONST T) :R2)) ;; on met à T pour la future comparaison dans le if
 		     (case op
 		    	('= `((JEQ (@,else_))))
 		    	('< `((JLT (@,else_))))
@@ -49,7 +49,7 @@
 		    	('>= `((JGE (@,else_))))
 		    	('/= `((JNE (@,else_))))
 		    )
-		    '((MOVE (:CONST nil) :R2));;si ça n'a pas jump (voir le compi-if)
+		    '((MOVE (CONST nil) :R2));;si ça n'a pas jump (voir le compi-if)
 		    `((@,else_))
 	)
 	)
@@ -58,23 +58,23 @@
 (defun compi-op (exp)
   (let ((operation (car exp))(argu (cdr exp)))
 	(append (compilation (list (car argu)) )
-		`((PUSH :R0))
+		`((PUSH R0))
 		(compilation (cdr argu))
-		`((PUSH :R0))
-		`((POP :R1))
-		`((POP :R0))
+		`((PUSH R0))
+		`((POP R1))
+		`((POP R0))
 		(case operation
-		  ('+ `((ADD :R1 :R0)))
-		  ('- `((SUB :R1 :R0)))
-		  ('* `((MULT :R1 :R0)))
-		  ('/ `((DIV :R1 :R0)))))     
+		  ('+ `((ADD R1 R0)))
+		  ('- `((SUB R1 R0)))
+		  ('* `((MULT R1 R0)))
+		  ('/ `((DIV R1 R0)))))     
 	) 
  )
 
 (defun compi-fonction(exp)
 	(let ((nb_param (length (cdr exp)))))
 	(append (compi-param-fonc (cdr exp))
-		`((PUSH (:CONST nb_param)))
+		`((PUSH (CONST nb_param)))
 		`((MOVE ))
 		`((MOVE ))
 		`((MOVE ))
@@ -88,7 +88,7 @@
 	(if (atom exp)
 		()
 		(append (compilation (car exp))
-				`((PUSH :R0))
+				`((PUSH R0))
 				(compi-param-fonc (cdr exp)))
 		)
 
@@ -96,16 +96,10 @@
 
 (defun compi-litt(exp)
 	(if (numberp  exp)
-		`((MOVE (:CONST ,exp) :R0))
-		`((MOVE (:REF , exp) :R0))
+		`((MOVE (CONST ,exp) R0))
+		`((MOVE (REF , exp) R0))
 	)
 )
-
-
-
-
-
-
 
 (defun equal_cas (exp inst)
   (eql (car exp) inst)
